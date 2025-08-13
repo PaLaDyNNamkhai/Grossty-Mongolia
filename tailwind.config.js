3ad8ea55eb48b286695b4c22b521b5b78f8a11d6
PaLaDyNNamkhai <bi888834@gmail.com>
2025-08-13 20:30:19 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}", // important for Next.js 13/14 app router
   ],
   theme: {
     extend: {},
